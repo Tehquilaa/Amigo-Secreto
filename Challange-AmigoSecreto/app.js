8ab25d963389e58a2be9382d0727436c3f5fffc5
Aldo <jimenezaldo155@gmail.com>
2025-01-25 20:46:33 -0600

    Creacion de la función agregarAmigo

--- Challange-AmigoSecreto/app.js
+++ Challange-AmigoSecreto/app.js
@@ -1,2 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+
+    let ingresarAmigo = document.getElementById("amigo").value
+   
+
+    if (ingresarAmigo == ''){
+        alert('Por favor ingrese el nombre de un amigo');
+
+    }else if(/\d/.test(ingresarAmigo)){
+        alert('Por favor ingrese un nombre válido');
+    }else{
+        amigos.push(ingresarAmigo)
+    }
+
+    document.querySelector('#amigo').value = '';
+    console.log(amigos);
+    return;
+}
